7ad32bfea8c228cf893379201679f0acbd25ee13
David <ballesterdavid2@gmail.com>
2025-07-24 09:25:15 +0200

    Creada aplicacion (solo base) para poder tener el espacio para la base de datos

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using DataBase.Components;
+using Radzen;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents(options =>
+    {
+        options.DetailedErrors = true;
+    });
+
+builder.Services.AddRadzenComponents();
+
+builder.Services.AddCascadingAuthenticationState();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+// Añade el "/database" a los archivos estaticos
+app.UsePathBase("/database");
+// Y asegura que el enrutamiento use ese path base
+app.UseRouting();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
